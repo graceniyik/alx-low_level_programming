e6ec4b982501862a7ee1b5d2018eda9dc649dba7
Graceniyik <niyikizagrace@gmail.com>
2023-02-17 09:57:25 -0800

    print combat

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int x;
+	int num;
 
-    for (x = 0; x < 10; x++)
-    {
-        printf("%d", x);
-        if (x < 9)
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
 
-    return (0);
+	putchar('\n');
+
+	return (0);
 }
